4db668e6183d2bb718985e7c82ae6d52990bb8a6
Charles C. Figueiredo <ccf@ccf.io>
2026-06-08 21:12:27 -0400

    feat(search): search() with hybrid/BM25 degradation ladder

--- src/cairn/search/__init__.py
+++ src/cairn/search/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
-from cairn.search.engine import Hit, hybrid_search, open_search, vector_search
+from cairn.search.engine import Hit, bm25_only, hybrid_search, open_search, search, vector_search
 
-__all__ = ["Hit", "hybrid_search", "open_search", "vector_search"]
+__all__ = ["Hit", "bm25_only", "hybrid_search", "open_search", "search", "vector_search"]
